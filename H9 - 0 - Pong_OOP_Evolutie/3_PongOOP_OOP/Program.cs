859c92b6b974f7b54b51c978c821c4652d341293
SvenJ-Atheneum <sven.janssens@atheneumsinttruiden.be>
2024-11-07 09:47:48 +0100

    Add files via upload

---
+++ H9 - 0 - Pong_OOP_Evolutie/3_PongOOP_OOP/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _3_PongOOP_OOP
+{    /* 
+     *  Verandering tegenover vorige?
+     *  
+     *  We plaatsen alle logica voor het balletje in een aparte klasse in het bestand Balletje.cs.
+     *  We vereenvoudigen de code in de main daardoor (het tweede balletje is even verdwenen en komt in volgende project terug)
+     *  Merk op dat nu onze loop veel leesbaarder wordt, daar alle "ingewikkelde" functionaliteit
+     *  verstopt (encapsulatie) zit in de Update methode van de klasse Balletje.
+     *
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Balletje bal1 = new Balletje();
+            bal1.X = 20;
+            bal1.Y = 20;
+            bal1.VectorX = 2;
+            bal1.VectorY = 1;
+
+            while (true)
+            {
+                bal1.Update();
+                bal1.TekenOpScherm();
+
+                System.Threading.Thread.Sleep(50);
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ H9 - 0 - Pong_OOP_Evolutie/5_OOPongMultiball_OOP/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _5_OOPongMultiball_OOP
+{    /* 
+     *  Verandering tegenover vorige?
+     *  
+     * In de klasse Balletje hebben we 1 aspect toegevoegd: een zogenaamde constructor. Deze methode 
+     * wordt enkel aangeroepen wanneer we een new object aanmaken. In het geval van Balletje zullen
+     * we in de constructor het object een willekeurige start-positie en vector geven.
+     * Merk op dat dit mogelijk stilstaande balletjes zal geven.
+     * 
+     * We voelen nu de kracht van OOP wanneer we 100 balletjes objecten in een array toevoegen.
+     *
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int AANTAL_BALLETJES = 100;
+            
+            Balletje[] veelBalletjes = new Balletje[AANTAL_BALLETJES];
+            for (int i = 0; i < veelBalletjes.Length; i++) //balletjes aanmaken
+            {
+                veelBalletjes[i] = new Balletje(); //zie klasse: extra constructor gemaakt die willekeurige startcoord kiest
+            }
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                for (int i = 0; i < veelBalletjes.Length; i++)
+                {
+                    veelBalletjes[i].Update(); //update alle balletjes
+                }
+                for (int i = 0; i < veelBalletjes.Length; i++)
+                {
+                    veelBalletjes[i].TekenOpScherm(); //teken alle balletjes
+                }
+                System.Threading.Thread.Sleep(50);
+                Console.Clear();
+            }
+        }
+    }
+}
